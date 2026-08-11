0niel/prime-agent

chore(deps): consolidate dependency updates (#632)
* chore(deps): consolidate dependency updates

* chore(deps): fold in remaining eligible Dependabot updates

Adds the reviewed bumps from #571, #599, #713, and #714 to the
consolidated update:

- brace-expansion 5.0.7 -> 5.0.9 (transitive, lockfile only)
- ip-address 10.2.0 -> 10.4.0 (transitive, lockfile only)
- typebox ^1.1.24 -> ^1.3.9 in packages/agent, packages/ai, and
  packages/coding-agent (lockfile resolves 1.3.10, released 2026-08-02,
  past the seven-day minimum release age)
- typescript ^5.9.2/^5.7.3 -> ^7.0.2 (dev-only; every build and check in
  this repo runs through tsgo, nothing invokes tsc)

undici 8.9.0 from #712 is intentionally NOT included: undici 8 requires
Node >= 22.19.0 while this package supports >= 22.8.0, so the major bump
would silently drop supported Node versions. It stays on 7.29.0.

packages/coding-agent/test/acp-kernel-features.test.ts
@@ -57,44 +57,43 @@ describe("ACP mode over a real IPython kernel", () => {
 		rmSync(tempDir, { recursive: true, force: true });
 	});
 
-	it(
-		"keeps IPython state across cells and represents each cell as an ACP execute call",
-		{ tags: ["kernel-heavy"], timeout: 180_000 },
-		async () => {
-			// Every provisioner in this file requests the same skill set: the kernel venv
-			// is shared, and a skill-less kernel here can leave a later skill-dependent
-			// test with an unsynced venv when files run concurrently.
-			provisioner = new IpythonKernelProvisioner(tempDir, { pythonSkills: [AGENT_MESSAGE_SKILL] });
-			const manager: KernelManager = await provisioner.ensure();
+	it("keeps IPython state across cells and represents each cell as an ACP execute call", {
+		tags: ["kernel-heavy"],
+		timeout: 180_000,
+	}, async () => {
+		// Every provisioner in this file requests the same skill set: the kernel venv
+		// is shared, and a skill-less kernel here can leave a later skill-dependent
+		// test with an unsynced venv when files run concurrently.
+		provisioner = new IpythonKernelProvisioner(tempDir, { pythonSkills: [AGENT_MESSAGE_SKILL] });
+		const manager: KernelManager = await provisioner.ensure();
 
-			const first = await manager.execute("acp_state = 41\nprint('set')");
-			expect(first.status).toBe("ok");
-			// Persistence is the whole point of the kernel: a second cell must see it.
-			const second = await manager.execute("print(acp_state + 1)");
-			expect(second.status).toBe("ok");
-			expect(second.stdout.trim()).toBe("42");
+		const first = await manager.execute("acp_state = 41\nprint('set')");
+		expect(first.status).toBe("ok");
+		// Persistence is the whole point of the kernel: a second cell must see it.
+		const second = await manager.execute("print(acp_state + 1)");
+		expect(second.status).toBe("ok");
+		expect(second.stdout.trim()).toBe("42");
 
-			const updates = acpUpdatesForSessionEvent(toolEndEvent("cell-2", second.stdout));
-			expect(updates[0]).toMatchObject({
-				sessionUpdate: "tool_call_update",
-				toolCallId: "cell-2",
-				status: "completed",
-			});
-			expect(JSON.stringify(updates[0]?.content)).toContain("42");
-		},
-	);
+		const updates = acpUpdatesForSessionEvent(toolEndEvent("cell-2", second.stdout));
+		expect(updates[0]).toMatchObject({
+			sessionUpdate: "tool_call_update",
+			toolCallId: "cell-2",
+			status: "completed",
+		});
+		expect(JSON.stringify(updates[0]?.content)).toContain("42");
+	});
 
-	it(
-		"runs continual-harness CRUD in the kernel and can represent the result over ACP",
-		{ tags: ["kernel-heavy"], timeout: 180_000 },
-		async () => {
-			provisioner = new IpythonKernelProvisioner(tempDir, {
-				pythonSkills: [AGENT_MESSAGE_SKILL],
-				env: { RLM_GLOBAL_HARNESS_STATE_DIR: join(tempDir, "harness") },
-			});
-			const manager = await provisioner.ensure();
+	it("runs continual-harness CRUD in the kernel and can represent the result over ACP", {
+		tags: ["kernel-heavy"],
+		timeout: 180_000,
+	}, async () => {
+		provisioner = new IpythonKernelProvisioner(tempDir, {
+			pythonSkills: [AGENT_MESSAGE_SKILL],
+			env: { RLM_GLOBAL_HARNESS_STATE_DIR: join(tempDir, "harness") },
+		});
+		const manager = await provisioner.ensure();
 
-			const allKinds = await manager.execute(`
+		const allKinds = await manager.execute(`
 import json
 mem = rlm.harness.create_memory(title="m", content="memory content", global_=True)
 note = rlm.harness.create_prompt_note(title="n", content="prompt note content", global_=True)
@@ -111,15 +110,15 @@ print(json.dumps({
     "skill_ref": skill.reference.get("import"),
 }, sort_keys=True))
 `);
-			expect(allKinds.status, why(allKinds)).toBe("ok");
-			// Every editable harness kind, not just memory: skills additionally require
-			// a python reference and an argument contract.
-			expect(JSON.parse(allKinds.stdout.trim())).toMatchObject({
-				kinds: ["memory", "prompt", "skill", "subagent"],
-				skill_ref: "pkg.mod",
-			});
+		expect(allKinds.status, why(allKinds)).toBe("ok");
+		// Every editable harness kind, not just memory: skills additionally require
+		// a python reference and an argument contract.
+		expect(JSON.parse(allKinds.stdout.trim())).toMatchObject({
+			kinds: ["memory", "prompt", "skill", "subagent"],
+			skill_ref: "pkg.mod",
+		});
 
-			const result = await manager.execute(`
+		const result = await manager.execute(`
 import json
 entry = rlm.harness.create_memory(
     title="ACP verification memory",
@@ -138,62 +137,61 @@ print(json.dumps({
     "after": after.title if after else None,
 }, sort_keys=True))
 `);
-			expect(result.status, why(result)).toBe("ok");
-			const payload = JSON.parse(result.stdout.trim());
-			expect(payload.found).toBe("ACP verification memory");
-			expect(payload.listed).toContain(payload.created);
-			expect(payload.deleted).toBe(true);
-			expect(payload.after).toBeNull();
+		expect(result.status, why(result)).toBe("ok");
+		const payload = JSON.parse(result.stdout.trim());
+		expect(payload.found).toBe("ACP verification memory");
+		expect(payload.listed).toContain(payload.created);
+		expect(payload.deleted).toBe(true);
+		expect(payload.after).toBeNull();
 
-			// A refinement outcome for that CRUD is expressible as namespaced metadata.
-			const refined = acpUpdatesForSessionEvent({
-				type: "refine_complete",
-				result: {
-					summary: "persisted ACP verification memory",
-					appliedEdits: [{ applied: true, action: "create", kind: "memory", id: payload.created }],
-				},
-			} as AgentConnectionSessionEvent);
-			expect(refined[0]?._meta).toMatchObject({
-				[PRIME_AGENT_META_NAMESPACE]: { refinement: { status: "complete" } },
-			});
-		},
-	);
+		// A refinement outcome for that CRUD is expressible as namespaced metadata.
+		const refined = acpUpdatesForSessionEvent({
+			type: "refine_complete",
+			result: {
+				summary: "persisted ACP verification memory",
+				appliedEdits: [{ applied: true, action: "create", kind: "memory", id: payload.created }],
+			},
+		} as AgentConnectionSessionEvent);
+		expect(refined[0]?._meta).toMatchObject({
+			[PRIME_AGENT_META_NAMESPACE]: { refinement: { status: "complete" } },
+		});
+	});
 
-	it(
-		"exposes rlm depth and subagent APIs to the kernel behind the ACP front end",
-		{ tags: ["kernel-heavy"], timeout: 180_000 },
-		async () => {
-			provisioner = new IpythonKernelProvisioner(tempDir, {
-				pythonSkills: [AGENT_MESSAGE_SKILL],
-				env: { RLM_DEPTH: "0", RLM_MAX_DEPTH: "1" },
-				hostHandlers: {
-					"rlm.list_subagents": async () => ({
-						subagents: [
-							{
-								rlm_child_id: "child-1",
-								active_session_id: "active-1",
-								session_id: "session-1",
-								session_name: "reviewer",
-								session_dir: tempDir,
-								status: "completed",
-							},
-						],
-					}),
-					"rlm.delete_subagent": async (payload) => ({
-						subagent: {
-							rlm_child_id: String(payload.target),
-							active_session_id: null,
+	it("exposes rlm depth and subagent APIs to the kernel behind the ACP front end", {
+		tags: ["kernel-heavy"],
+		timeout: 180_000,
+	}, async () => {
+		provisioner = new IpythonKernelProvisioner(tempDir, {
+			pythonSkills: [AGENT_MESSAGE_SKILL],
+			env: { RLM_DEPTH: "0", RLM_MAX_DEPTH: "1" },
+			hostHandlers: {
+				"rlm.list_subagents": async () => ({
+					subagents: [
+						{
+							rlm_child_id: "child-1",
+							active_session_id: "active-1",
 							session_id: "session-1",
 							session_name: "reviewer",
 							session_dir: tempDir,
 							status: "completed",
 						},
-					}),
-				},
-			});
-			const manager = await provisioner.ensure();
+					],
+				}),
+				"rlm.delete_subagent": async (payload) => ({
+					subagent: {
+						rlm_child_id: String(payload.target),
+						active_session_id: null,
+						session_id: "session-1",
+						session_name: "reviewer",
+						session_dir: tempDir,
+						status: "completed",
+					},
+				}),
+			},
+		});
+		const manager = await provisioner.ensure();
 
-			const result = await manager.execute(`
+		const result = await manager.execute(`
 import json, os
 children = await rlm.list_subagents()
 removed = await rlm.delete_subagent(children[0])
@@ -204,53 +202,50 @@ print(json.dumps({
     "removed": removed.session_name,
 }, sort_keys=True))
 `);
-			expect(result.status, why(result)).toBe("ok");
-			const payload = JSON.parse(result.stdout.trim());
-			expect(payload.names).toEqual(["reviewer"]);
-			expect(payload.removed).toBe("reviewer");
-			expect(payload.depth).toBe("0");
-			expect(payload.max_depth).toBe("1");
-		},
-	);
+		expect(result.status, why(result)).toBe("ok");
+		const payload = JSON.parse(result.stdout.trim());
+		expect(payload.names).toEqual(["reviewer"]);
+		expect(payload.removed).toBe("reviewer");
+		expect(payload.depth).toBe("0");
+		expect(payload.max_depth).toBe("1");
+	});
 
-	it(
-		"sends an agent-to-agent message from the kernel and surfaces it over ACP",
-		{ tags: ["kernel-heavy"], timeout: 180_000 },
-		async () => {
-			provisioner = new IpythonKernelProvisioner(tempDir, {
-				pythonSkills: [AGENT_MESSAGE_SKILL],
-				hostHandlers: {
-					// The family roster: parent, siblings, and children of this agent.
-					"agent_message.list_agents": async () => ({
-						current: { name: "root", id: "session-alpha", depth: 0 },
-						entries: [{ relationship: "child", name: "reviewer", id: "session-beta", depth: 1, status: "idle" }],
-					}),
-					"agent_message.send": async (payload) => ({
-						id: "agentmsg-acp",
-						source: "agent_message",
-						target: { activeSessionId: "beta", sessionId: "session-beta", sessionName: "reviewer" },
-						message: payload.message,
-						deliveryStatus: "queued",
-						queuedAt: "2026-08-04T00:00:00.000Z",
-						deliveryMode: payload.mode ?? "auto",
-					}),
-				},
-			});
-			const manager = await provisioner.ensure();
+	it("sends an agent-to-agent message from the kernel and surfaces it over ACP", {
+		tags: ["kernel-heavy"],
+		timeout: 180_000,
+	}, async () => {
+		provisioner = new IpythonKernelProvisioner(tempDir, {
+			pythonSkills: [AGENT_MESSAGE_SKILL],
+			hostHandlers: {
+				// The family roster: parent, siblings, and children of this agent.
+				"agent_message.list_agents": async () => ({
+					current: { name: "root", id: "session-alpha", depth: 0 },
+					entries: [{ relationship: "child", name: "reviewer", id: "session-beta", depth: 1, status: "idle" }],
+				}),
+				"agent_message.send": async (payload) => ({
+					id: "agentmsg-acp",
+					source: "agent_message",
+					target: { activeSessionId: "beta", sessionId: "session-beta", sessionName: "reviewer" },
+					message: payload.message,
+					deliveryStatus: "queued",
+					queuedAt: "2026-08-04T00:00:00.000Z",
+					deliveryMode: payload.mode ?? "auto",
+				}),
+			},
+		});
+		const manager = await provisioner.ensure();
 
-			// The kernel venv is shared across test files. If a concurrently running
-			// file rebuilt it without this skill, say so plainly rather than failing
-			// later with an opaque AttributeError on the stub object.
-			const available = await manager.execute(
-				"import json; print(json.dumps({'kind': type(agent_message).__name__}))",
-			);
-			expect(available.status, why(available)).toBe("ok");
-			expect(
-				JSON.parse(available.stdout.trim()).kind,
-				"agent_message skill was not installed into the shared kernel venv",
-			).not.toBe("_PrimeAgentUnavailableSkill");
+		// The kernel venv is shared across test files. If a concurrently running
+		// file rebuilt it without this skill, say so plainly rather than failing
+		// later with an opaque AttributeError on the stub object.
+		const available = await manager.execute("import json; print(json.dumps({'kind': type(agent_message).__name__}))");
+		expect(available.status, why(available)).toBe("ok");
+		expect(
+			JSON.parse(available.stdout.trim()).kind,
+			"agent_message skill was not installed into the shared kernel venv",
+		).not.toBe("_PrimeAgentUnavailableSkill");
 
-			const result = await manager.execute(`
+		const result = await manager.execute(`
 import json
 roster = await agent_message.list_agents()
 receipt = await agent_message.send("status update", receiver_role="child", receiver_name="reviewer")
@@ -259,23 +254,22 @@ print(json.dumps({
     "status": receipt["deliveryStatus"],
 }))
 `);
-			expect(result.status, why(result)).toBe("ok");
-			const payload = JSON.parse(result.stdout.trim());
-			expect(payload.roster).toEqual(["reviewer"]);
-			expect(payload.status).toBe("queued");
+		expect(result.status, why(result)).toBe("ok");
+		const payload = JSON.parse(result.stdout.trim());
+		expect(payload.roster).toEqual(["reviewer"]);
+		expect(payload.status).toBe("queued");
 
-			// The kernel reports the send; ACP carries it as namespaced metadata.
-			const sentMessages = result.sentAgentMessages ?? [];
-			expect(sentMessages.length).toBeGreaterThan(0);
-			const sent = sentMessages[0];
-			const updates = acpUpdatesForSessionEvent({
-				type: "ipython_sent_agent_message",
-				toolCallId: "cell-msg",
-				message: sent,
-			} as AgentConnectionSessionEvent);
-			expect(updates[0]?._meta).toMatchObject({
-				[PRIME_AGENT_META_NAMESPACE]: { agentMessage: { toolCallId: "cell-msg", deliveryStatus: "queued" } },
-			});
-		},
-	);
+		// The kernel reports the send; ACP carries it as namespaced metadata.
+		const sentMessages = result.sentAgentMessages ?? [];
+		expect(sentMessages.length).toBeGreaterThan(0);
+		const sent = sentMessages[0];
+		const updates = acpUpdatesForSessionEvent({
+			type: "ipython_sent_agent_message",
+			toolCallId: "cell-msg",
+			message: sent,
+		} as AgentConnectionSessionEvent);
+		expect(updates[0]?._meta).toMatchObject({
+			[PRIME_AGENT_META_NAMESPACE]: { agentMessage: { toolCallId: "cell-msg", deliveryStatus: "queued" } },
+		});
+	});
 });

0oAstro/fulcrum

packages/coding-agent/test/acp-kernel-features.test.ts
@@ -57,44 +57,43 @@ describe("ACP mode over a real IPython kernel", () => {
 		rmSync(tempDir, { recursive: true, force: true });
 	});
 
-	it(
-		"keeps IPython state across cells and represents each cell as an ACP execute call",
-		{ tags: ["kernel-heavy"], timeout: 180_000 },
-		async () => {
-			// Every provisioner in this file requests the same skill set: the kernel venv
-			// is shared, and a skill-less kernel here can leave a later skill-dependent
-			// test with an unsynced venv when files run concurrently.
-			provisioner = new IpythonKernelProvisioner(tempDir, { pythonSkills: [AGENT_MESSAGE_SKILL] });
-			const manager: KernelManager = await provisioner.ensure();
+	it("keeps IPython state across cells and represents each cell as an ACP execute call", {
+		tags: ["kernel-heavy"],
+		timeout: 180_000,
+	}, async () => {
+		// Every provisioner in this file requests the same skill set: the kernel venv
+		// is shared, and a skill-less kernel here can leave a later skill-dependent
+		// test with an unsynced venv when files run concurrently.
+		provisioner = new IpythonKernelProvisioner(tempDir, { pythonSkills: [AGENT_MESSAGE_SKILL] });
+		const manager: KernelManager = await provisioner.ensure();
 
-			const first = await manager.execute("acp_state = 41\nprint('set')");
-			expect(first.status).toBe("ok");
-			// Persistence is the whole point of the kernel: a second cell must see it.
-			const second = await manager.execute("print(acp_state + 1)");
-			expect(second.status).toBe("ok");
-			expect(second.stdout.trim()).toBe("42");
+		const first = await manager.execute("acp_state = 41\nprint('set')");
+		expect(first.status).toBe("ok");
+		// Persistence is the whole point of the kernel: a second cell must see it.
+		const second = await manager.execute("print(acp_state + 1)");
+		expect(second.status).toBe("ok");
+		expect(second.stdout.trim()).toBe("42");
 
-			const updates = acpUpdatesForSessionEvent(toolEndEvent("cell-2", second.stdout));
-			expect(updates[0]).toMatchObject({
-				sessionUpdate: "tool_call_update",
-				toolCallId: "cell-2",
-				status: "completed",
-			});
-			expect(JSON.stringify(updates[0]?.content)).toContain("42");
-		},
-	);
+		const updates = acpUpdatesForSessionEvent(toolEndEvent("cell-2", second.stdout));
+		expect(updates[0]).toMatchObject({
+			sessionUpdate: "tool_call_update",
+			toolCallId: "cell-2",
+			status: "completed",
+		});
+		expect(JSON.stringify(updates[0]?.content)).toContain("42");
+	});
 
-	it(
-		"runs continual-harness CRUD in the kernel and can represent the result over ACP",
-		{ tags: ["kernel-heavy"], timeout: 180_000 },
-		async () => {
-			provisioner = new IpythonKernelProvisioner(tempDir, {
-				pythonSkills: [AGENT_MESSAGE_SKILL],
-				env: { RLM_GLOBAL_HARNESS_STATE_DIR: join(tempDir, "harness") },
-			});
-			const manager = await provisioner.ensure();
+	it("runs continual-harness CRUD in the kernel and can represent the result over ACP", {
+		tags: ["kernel-heavy"],
+		timeout: 180_000,
+	}, async () => {
+		provisioner = new IpythonKernelProvisioner(tempDir, {
+			pythonSkills: [AGENT_MESSAGE_SKILL],
+			env: { RLM_GLOBAL_HARNESS_STATE_DIR: join(tempDir, "harness") },
+		});
+		const manager = await provisioner.ensure();
 
-			const allKinds = await manager.execute(`
+		const allKinds = await manager.execute(`
 import json
 mem = rlm.harness.create_memory(title="m", content="memory content", global_=True)
 note = rlm.harness.create_prompt_note(title="n", content="prompt note content", global_=True)
@@ -111,15 +110,15 @@ print(json.dumps({
     "skill_ref": skill.reference.get("import"),
 }, sort_keys=True))
 `);
-			expect(allKinds.status, why(allKinds)).toBe("ok");
-			// Every editable harness kind, not just memory: skills additionally require
-			// a python reference and an argument contract.
-			expect(JSON.parse(allKinds.stdout.trim())).toMatchObject({
-				kinds: ["memory", "prompt", "skill", "subagent"],
-				skill_ref: "pkg.mod",
-			});
+		expect(allKinds.status, why(allKinds)).toBe("ok");
+		// Every editable harness kind, not just memory: skills additionally require
+		// a python reference and an argument contract.
+		expect(JSON.parse(allKinds.stdout.trim())).toMatchObject({
+			kinds: ["memory", "prompt", "skill", "subagent"],
+			skill_ref: "pkg.mod",
+		});
 
-			const result = await manager.execute(`
+		const result = await manager.execute(`
 import json
 entry = rlm.harness.create_memory(
     title="ACP verification memory",
@@ -138,62 +137,61 @@ print(json.dumps({
     "after": after.title if after else None,
 }, sort_keys=True))
 `);
-			expect(result.status, why(result)).toBe("ok");
-			const payload = JSON.parse(result.stdout.trim());
-			expect(payload.found).toBe("ACP verification memory");
-			expect(payload.listed).toContain(payload.created);
-			expect(payload.deleted).toBe(true);
-			expect(payload.after).toBeNull();
+		expect(result.status, why(result)).toBe("ok");
+		const payload = JSON.parse(result.stdout.trim());
+		expect(payload.found).toBe("ACP verification memory");
+		expect(payload.listed).toContain(payload.created);
+		expect(payload.deleted).toBe(true);
+		expect(payload.after).toBeNull();
 
-			// A refinement outcome for that CRUD is expressible as namespaced metadata.
-			const refined = acpUpdatesForSessionEvent({
-				type: "refine_complete",
-				result: {
-					summary: "persisted ACP verification memory",
-					appliedEdits: [{ applied: true, action: "create", kind: "memory", id: payload.created }],
-				},
-			} as AgentConnectionSessionEvent);
-			expect(refined[0]?._meta).toMatchObject({
-				[FULCRUM_META_NAMESPACE]: { refinement: { status: "complete" } },
-			});
-		},
-	);
+		// A refinement outcome for that CRUD is expressible as namespaced metadata.
+		const refined = acpUpdatesForSessionEvent({
+			type: "refine_complete",
+			result: {
+				summary: "persisted ACP verification memory",
+				appliedEdits: [{ applied: true, action: "create", kind: "memory", id: payload.created }],
+			},
+		} as AgentConnectionSessionEvent);
+		expect(refined[0]?._meta).toMatchObject({
+			[FULCRUM_META_NAMESPACE]: { refinement: { status: "complete" } },
+		});
+	});
 
-	it(
-		"exposes rlm depth and subagent APIs to the kernel behind the ACP front end",
-		{ tags: ["kernel-heavy"], timeout: 180_000 },
-		async () => {
-			provisioner = new IpythonKernelProvisioner(tempDir, {
-				pythonSkills: [AGENT_MESSAGE_SKILL],
-				env: { RLM_DEPTH: "0", RLM_MAX_DEPTH: "1" },
-				hostHandlers: {
-					"rlm.list_subagents": async () => ({
-						subagents: [
-							{
-								rlm_child_id: "child-1",
-								active_session_id: "active-1",
-								session_id: "session-1",
-								session_name: "reviewer",
-								session_dir: tempDir,
-								status: "completed",
-							},
-						],
-					}),
-					"rlm.delete_subagent": async (payload) => ({
-						subagent: {
-							rlm_child_id: String(payload.target),
-							active_session_id: null,
+	it("exposes rlm depth and subagent APIs to the kernel behind the ACP front end", {
+		tags: ["kernel-heavy"],
+		timeout: 180_000,
+	}, async () => {
+		provisioner = new IpythonKernelProvisioner(tempDir, {
+			pythonSkills: [AGENT_MESSAGE_SKILL],
+			env: { RLM_DEPTH: "0", RLM_MAX_DEPTH: "1" },
+			hostHandlers: {
+				"rlm.list_subagents": async () => ({
+					subagents: [
+						{
+							rlm_child_id: "child-1",
+							active_session_id: "active-1",
 							session_id: "session-1",
 							session_name: "reviewer",
 							session_dir: tempDir,
 							status: "completed",
 						},
-					}),
-				},
-			});
-			const manager = await provisioner.ensure();
+					],
+				}),
+				"rlm.delete_subagent": async (payload) => ({
+					subagent: {
+						rlm_child_id: String(payload.target),
+						active_session_id: null,
+						session_id: "session-1",
+						session_name: "reviewer",
+						session_dir: tempDir,
+						status: "completed",
+					},
+				}),
+			},
+		});
+		const manager = await provisioner.ensure();
 
-			const result = await manager.execute(`
+		const result = await manager.execute(`
 import json, os
 children = await rlm.list_subagents()
 removed = await rlm.delete_subagent(children[0])
@@ -204,53 +202,50 @@ print(json.dumps({
     "removed": removed.session_name,
 }, sort_keys=True))
 `);
-			expect(result.status, why(result)).toBe("ok");
-			const payload = JSON.parse(result.stdout.trim());
-			expect(payload.names).toEqual(["reviewer"]);
-			expect(payload.removed).toBe("reviewer");
-			expect(payload.depth).toBe("0");
-			expect(payload.max_depth).toBe("1");
-		},
-	);
+		expect(result.status, why(result)).toBe("ok");
+		const payload = JSON.parse(result.stdout.trim());
+		expect(payload.names).toEqual(["reviewer"]);
+		expect(payload.removed).toBe("reviewer");
+		expect(payload.depth).toBe("0");
+		expect(payload.max_depth).toBe("1");
+	});
 
-	it(
-		"sends an agent-to-agent message from the kernel and surfaces it over ACP",
-		{ tags: ["kernel-heavy"], timeout: 180_000 },
-		async () => {
-			provisioner = new IpythonKernelProvisioner(tempDir, {
-				pythonSkills: [AGENT_MESSAGE_SKILL],
-				hostHandlers: {
-					// The family roster: parent, siblings, and children of this agent.
-					"agent_message.list_agents": async () => ({
-						current: { name: "root", id: "session-alpha", depth: 0 },
-						entries: [{ relationship: "child", name: "reviewer", id: "session-beta", depth: 1, status: "idle" }],
-					}),
-					"agent_message.send": async (payload) => ({
-						id: "agentmsg-acp",
-						source: "agent_message",
-						target: { activeSessionId: "beta", sessionId: "session-beta", sessionName: "reviewer" },
-						message: payload.message,
-						deliveryStatus: "queued",
-						queuedAt: "2026-08-04T00:00:00.000Z",
-						deliveryMode: payload.mode ?? "auto",
-					}),
-				},
-			});
-			const manager = await provisioner.ensure();
+	it("sends an agent-to-agent message from the kernel and surfaces it over ACP", {
+		tags: ["kernel-heavy"],
+		timeout: 180_000,
+	}, async () => {
+		provisioner = new IpythonKernelProvisioner(tempDir, {
+			pythonSkills: [AGENT_MESSAGE_SKILL],
+			hostHandlers: {
+				// The family roster: parent, siblings, and children of this agent.
+				"agent_message.list_agents": async () => ({
+					current: { name: "root", id: "session-alpha", depth: 0 },
+					entries: [{ relationship: "child", name: "reviewer", id: "session-beta", depth: 1, status: "idle" }],
+				}),
+				"agent_message.send": async (payload) => ({
+					id: "agentmsg-acp",
+					source: "agent_message",
+					target: { activeSessionId: "beta", sessionId: "session-beta", sessionName: "reviewer" },
+					message: payload.message,
+					deliveryStatus: "queued",
+					queuedAt: "2026-08-04T00:00:00.000Z",
+					deliveryMode: payload.mode ?? "auto",
+				}),
+			},
+		});
+		const manager = await provisioner.ensure();
 
-			// The kernel venv is shared across test files. If a concurrently running
-			// file rebuilt it without this skill, say so plainly rather than failing
-			// later with an opaque AttributeError on the stub object.
-			const available = await manager.execute(
-				"import json; print(json.dumps({'kind': type(agent_message).__name__}))",
-			);
-			expect(available.status, why(available)).toBe("ok");
-			expect(
-				JSON.parse(available.stdout.trim()).kind,
-				"agent_message skill was not installed into the shared kernel venv",
-			).not.toBe("_FulcrumUnavailableSkill");
+		// The kernel venv is shared across test files. If a concurrently running
+		// file rebuilt it without this skill, say so plainly rather than failing
+		// later with an opaque AttributeError on the stub object.
+		const available = await manager.execute("import json; print(json.dumps({'kind': type(agent_message).__name__}))");
+		expect(available.status, why(available)).toBe("ok");
+		expect(
+			JSON.parse(available.stdout.trim()).kind,
+			"agent_message skill was not installed into the shared kernel venv",
+		).not.toBe("_FulcrumUnavailableSkill");
 
-			const result = await manager.execute(`
+		const result = await manager.execute(`
 import json
 roster = await agent_message.list_agents()
 receipt = await agent_message.send("status update", receiver_role="child", receiver_name="reviewer")
@@ -259,23 +254,22 @@ print(json.dumps({
     "status": receipt["deliveryStatus"],
 }))
 `);
-			expect(result.status, why(result)).toBe("ok");
-			const payload = JSON.parse(result.stdout.trim());
-			expect(payload.roster).toEqual(["reviewer"]);
-			expect(payload.status).toBe("queued");
+		expect(result.status, why(result)).toBe("ok");
+		const payload = JSON.parse(result.stdout.trim());
+		expect(payload.roster).toEqual(["reviewer"]);
+		expect(payload.status).toBe("queued");
 
-			// The kernel reports the send; ACP carries it as namespaced metadata.
-			const sentMessages = result.sentAgentMessages ?? [];
-			expect(sentMessages.length).toBeGreaterThan(0);
-			const sent = sentMessages[0];
-			const updates = acpUpdatesForSessionEvent({
-				type: "ipython_sent_agent_message",
-				toolCallId: "cell-msg",
-				message: sent,
-			} as AgentConnectionSessionEvent);
-			expect(updates[0]?._meta).toMatchObject({
-				[FULCRUM_META_NAMESPACE]: { agentMessage: { toolCallId: "cell-msg", deliveryStatus: "queued" } },
-			});
-		},
-	);
+		// The kernel reports the send; ACP carries it as namespaced metadata.
+		const sentMessages = result.sentAgentMessages ?? [];
+		expect(sentMessages.length).toBeGreaterThan(0);
+		const sent = sentMessages[0];
+		const updates = acpUpdatesForSessionEvent({
+			type: "ipython_sent_agent_message",
+			toolCallId: "cell-msg",
+			message: sent,
+		} as AgentConnectionSessionEvent);
+		expect(updates[0]?._meta).toMatchObject({
+			[FULCRUM_META_NAMESPACE]: { agentMessage: { toolCallId: "cell-msg", deliveryStatus: "queued" } },
+		});
+	});
 });